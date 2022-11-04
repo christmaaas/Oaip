f03cec01abcd4372f8a7ffb451344179fd17d6f5
artemslinko <112775101+artemslinko@users.noreply.github.com>
2022-11-04 07:26:09 +0300

    Laba 5. Zadanie 2

---
+++ ''Laba 5. Zadanie 2''/Laba 5. Zadanie 2/arrays.h
@@ -0,0 +1,7 @@
+#pragma once
+int** memory(int sizeofrows, int sizeofcols);
+void array_input_random(int** mas, int sizeofrows, int sizeofcols);
+void array_input_manually(int** mas, int sizeofrows, int sizeofcols);
+void print_array(int** array, int sizeofrows, int sizeofcols);
+int array_size_input(int arraysize);
+int choice_0_or_1(int choice);
