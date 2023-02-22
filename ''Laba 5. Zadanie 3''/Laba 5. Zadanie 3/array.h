03da2eaf942c4e4a0d2974da99763fb727cb195f
artemslinko <112775101+artemslinko@users.noreply.github.com>
2023-02-22 03:24:02 +0300

    Delete ''Laba 5. Zadanie 3'' directory

--- ''Laba 5. Zadanie 3''/Laba 5. Zadanie 3/array.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-int  delete_after_element(int element_delete_after);
-void print_array(int** array, int sizeofrows);
-int array_size_input(int arraysize);
-void allocate_memory_one_element(int** array, int sizeofrows);
-void input_array(int** array, int sizeofrows);
-void delete_element(int** array, int sizeofrows, int element_delete_after);
