539a8e45e615bd51a27725d68d02e4600c210656
artemslinko <112775101+artemslinko@users.noreply.github.com>
2023-02-22 03:26:40 +0300

    Delete Laba 5. Zadanie 1 directory

--- Laba 5. Zadanie 1/Laba 5. Zadanie 1/arrays.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-void create_array(int** array, int arraysize);
-void delete_prime_number(int** array, int* arraysize);
-void array_otput(int* array, int arraysize);
-void array_input(int* array, int arraysize);
-int array_size_input(int arraysize);
