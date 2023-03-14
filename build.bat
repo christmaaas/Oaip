c3b0a9600cfdbc1db386a61b0081f97ef2da84fa
artemslinko <112775101+artemslinko@users.noreply.github.com>
2023-03-14 20:32:36 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-msbuild  OAIP\LABA2.SEM2\Ziper\Zipper\Zipper.vcxproj
-msbuild  OAIP\LABA2.SEM2\Unarchiver\Unarchiver\Unarchiver.vcxproj
+msbuild  LABA2.SEM2\Ziper\Zipper\Zipper.vcxproj
+msbuild  LABA2.SEM2\Unarchiver\Unarchiver\Unarchiver.vcxproj
