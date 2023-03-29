1ead1003e35a334502cc7792b8e685132a15bfdb
artemslinko <112775101+artemslinko@users.noreply.github.com>
2023-03-29 09:13:59 +0300

    Delete LABA3 directory

--- LABA3/LABA3/formulas.c
+++
@@ -1,6 +0,0 @@
-#include"formulas.h"
-
-char set_bw_color(char red, char green, char blue) {
-	char bw = (char)(red * 0.3 + green * 0.59 + blue * 0.11);
-	return bw;
-}
--- LABA3/LABA3/formulas.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include<stdio.h>
-#include<stdlib.h>
-
-char set_bw_color(char red, char green, char blue);
