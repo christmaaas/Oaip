bffc5a31256d9a1d86a56be3ee7ed2dfabe99b78
artemslinko <112775101+artemslinko@users.noreply.github.com>
2023-04-25 22:47:35 +0300

    Add files via upload

---
+++ LABA5.SEM2/LABA5.SEM2/hashmap.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SIZE_OF_HASHTABLE 50000
---
+++ LABA5.SEM2/LABA5.SEM2/main.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+
+#include "hashmap.h"
+
+int main() {
+
+
+	return 0;
+}
