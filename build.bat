4f8fb12084625be6abb689cae6fe507258942028
Artem Slinko <112775101+artemslinko@users.noreply.github.com>
2023-05-14 13:49:08 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,1 +1,6 @@
+msbuild  LABA1\LABA1\LABA1.vcxproj
+msbuild  LABA2.SEM2\Unarchiver\Unarchiver\Unarchiver.vcxproj
+msbuild  LABA2.SEM2\Ziper\Zipper\Zipper.vcxproj
 msbuild  LABA3.SEM2\LABA3.SEM2\LABA3.SEM2.vcxproj
+msbuild  LABA4.SEM2\LABA4.SEM2\LABA4.SEM2.vcxproj
+msbuild  LABA5.SEM2\LABA5.SEM2\LABA5.SEM2.vcxproj
